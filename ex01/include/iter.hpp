0c60867b4eebfaa41170960c85b4351ac3c85289
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2025-09-16 12:51:55 +0200

    std::size_t -> size_t

--- ex01/include/iter.hpp
+++ ex01/include/iter.hpp
@@ -7,14 +7,14 @@
 // Template function to apply a function to each element of an array
 template <typename T, typename Func>
 void iter(T* arr, size_t len, Func func) {
-	for (std::size_t i = 0; i < len; ++i)
+	for (size_t i = 0; i < len; ++i)
 		func(arr[i]);
 }
 
 // Overload for const arrays
 template <typename T, typename Func>
 void iter(const T* arr, size_t len, Func func) {
-	for (std::size_t i = 0; i < len; ++i)
+	for (size_t i = 0; i < len; ++i)
 		func(arr[i]);
 }
 #endif
